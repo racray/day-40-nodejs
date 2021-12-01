f27c7cdbba6fecf5aba6221630a4ad6ac5cd96e7
racray <rayedkhan007@gmail.com>
2021-12-01 17:04:38 +0530

    d

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const express = require('express')
-const dotenv = require('dotenv');
-const app = express();
-
 const fs = require('fs');
 
-dotenv.config();
-
-const PORT = process.env.PORT;
-
-
-
-app.get("/",async (request,response)=>{
-
-    const fs = require('fs');
-    const date = +new Date;
-    const date2 = date.toString();
-    console.log(date2);
-    fs.writeFile('./backup/current-date-time.txt',date2,err=>console.log("completed"));
-
-    fs.readFile('./backup/current-date-time.txt',"utf-8",(err,data)=>console.log(data));
+const date = +new Date;
+const date2 = date.toString();
+console.log(date2);
+fs.writeFile('./backup/current-date-time.txt',date2,err=>console.log("completed"));
+fs.readFile('./backup/current-date-time.txt',"utf-8",(err,data)=>console.log(data));
 
 
-})
 
-app.listen(PORT,()=>console.log("app started",PORT));
