b902fc6c73fcef737936124f01509e6b22bc5798
racray <rayedkhan007@gmail.com>
2021-12-01 16:52:06 +0530

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/",async (request,response)=>{
     fs.writeFile('./backup/current-date-time.txt',date2,err=>console.log("completed"));
 
     fs.readFile('./backup/current-date-time.txt',"utf-8",(err,data)=>console.log(data));
-
+    
 
 })
 
