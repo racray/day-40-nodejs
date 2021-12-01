7a7f558c4b2d0f774655d084142b20fb6af3c6b4
racray <rayedkhan007@gmail.com>
2021-12-01 17:00:59 +0530

    lion

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get("/",async (request,response)=>{
     const fs = require('fs');
     const date = +new Date;
     const date2 = date.toString();
+    console.log(date2);
     fs.writeFile('./backup/current-date-time.txt',date2,err=>console.log("completed"));
 
     fs.readFile('./backup/current-date-time.txt',"utf-8",(err,data)=>console.log(data));
